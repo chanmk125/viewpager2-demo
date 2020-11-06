31040eeaf3b580bc36a426b7e2b328f3d2437dba
chanmk125 <4ex21ia@gmail.com>
2020-11-07 02:13:02 +0900

    Initial commit

---
+++ app/src/main/java/com/example/viewpagerdemo/ScreenSlidePageActivity.kt
@@ -0,0 +1,71 @@
+package com.example.viewpagerdemo
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_screen_slide.*
+
+/**
+ * The number of pages (wizard steps) to show in this demo.
+ */
+private const val NUM_PAGES = 5
+
+// For logging.
+const val TAG = "testing"
+
+class ScreenSlidePagerActivity : FragmentActivity() {
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private lateinit var viewPager: ViewPager2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen_slide)
+
+        // Instantiate a ViewPager2 and a PagerAdapter.
+        viewPager = findViewById(R.id.pager)
+
+        // The pager adapter, which provides the pages to the view pager widget.
+        val pagerAdapter = ScreenSlidePagerAdapter(this)
+        viewPager.adapter = pagerAdapter
+
+        // Register a callback listener from a ViewPager2.
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                page_number.text = getString(R.string.lorem_ipsum, position)
+            }
+        })
+
+        // For testing, access an instance of a page number.
+        Log.d(TAG, "${page_number.accessibilityClassName}")
+    }
+
+    override fun onBackPressed() {
+        if (viewPager.currentItem == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed()
+        } else {
+            // Otherwise, select the previous step.
+            viewPager.currentItem = viewPager.currentItem - 1
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = NUM_PAGES
+
+        override fun createFragment(position: Int): Fragment =
+            ScreenSlidePageFragment.newIntent(position)
+    }
+}
---
+++ app/src/main/java/com/example/viewpagerdemo/ScreenSlidePageFragment.kt
@@ -0,0 +1,39 @@
+package com.example.viewpagerdemo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_screen_slide_page.*
+
+class ScreenSlidePageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_screen_slide_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+            page_number.text = it.getInt(ARGS_PAGER_POSITION).toString()
+        }
+    }
+
+    companion object {
+        private const val ARGS_PAGER_POSITION = "args_pager_position"
+
+        @JvmStatic
+        fun newIntent(position: Int): ScreenSlidePageFragment {
+            return ScreenSlidePageFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARGS_PAGER_POSITION, position)
+                }
+            }
+        }
+    }
+}
