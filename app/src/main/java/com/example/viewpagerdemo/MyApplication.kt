680da0400eacf167e5ce3ca92270ff53e884a155
mrky125 <4ex21ia@gmail.com>
2021-04-07 00:50:39 +0900

    add application that create all instances related dependencies

---
+++ app/src/main/java/com/example/viewpagerdemo/MyApplication.kt
@@ -0,0 +1,20 @@
+package com.example.viewpagerdemo
+
+import android.app.Application
+import com.example.viewpagerdemo.di.GitHubContainer
+
+class MyApplication : Application() {
+
+    companion object {
+        lateinit var gitHubContainer: GitHubContainer
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        initContainer()
+    }
+
+    private fun initContainer() {
+        gitHubContainer = GitHubContainer()
+    }
+}
