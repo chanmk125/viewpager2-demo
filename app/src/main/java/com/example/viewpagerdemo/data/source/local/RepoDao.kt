fbf37f25dd6c5fe5c5c47d39c8a4f99b186f0301
mrky125 <4ex21ia@gmail.com>
2021-04-07 02:03:56 +0900

    chg suspend fun in dao

--- app/src/main/java/com/example/viewpagerdemo/data/source/local/RepoDao.kt
+++ app/src/main/java/com/example/viewpagerdemo/data/source/local/RepoDao.kt
@@ -9,11 +9,11 @@ import com.example.viewpagerdemo.data.model.GitHubRepo
 @Dao
 interface RepoDao {
     @Query("SELECT * FROM github_repos")
-    fun getAll(): List<GitHubRepo>
+    suspend fun getAll(): List<GitHubRepo>
 
     @Insert
-    fun insertAll(repos: List<GitHubRepo>)
+    suspend fun insertAll(repos: List<GitHubRepo>)
 
     @Delete
-    fun delete(repo: GitHubRepo)
+    suspend fun delete(repo: GitHubRepo)
 }
