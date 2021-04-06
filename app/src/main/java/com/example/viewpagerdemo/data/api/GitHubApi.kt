6dd818477bd9eb9c4bf152039ee42b0efca8e953
mrky125 <4ex21ia@gmail.com>
2021-04-07 00:36:18 +0900

    add api interface of endpoint

---
+++ app/src/main/java/com/example/viewpagerdemo/data/api/GitHubApi.kt
@@ -0,0 +1,21 @@
+package com.example.viewpagerdemo.data.api
+
+import com.example.viewpagerdemo.data.model.GitHubOwner
+import com.example.viewpagerdemo.data.model.GitHubRepo
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface GitHubApi {
+
+    @GET("/users/{username}/repos")
+    suspend fun getGithubRepos(
+        @Path("username") username: String,
+        @Query("page") page: Int,
+        @Query("per_page") perPage: Int
+    ): Response<List<GitHubRepo>>
+
+    @GET("/users/{username}")
+    suspend fun getGithubOwner(@Path("username") username: String): Response<GitHubOwner>
+}
