b141f3e9e68f4b9e02ae6dc16954790272e278f8
mrky125 <4ex21ia@gmail.com>
2021-04-07 01:26:06 +0900

    add entity and column name

--- app/src/main/java/com/example/viewpagerdemo/data/model/GitHubRepo.kt
+++ app/src/main/java/com/example/viewpagerdemo/data/model/GitHubRepo.kt
@@ -1,13 +1,27 @@
 package com.example.viewpagerdemo.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.squareup.moshi.Json
 
+@Entity(tableName = "github_repos")
 class GitHubRepo (
+    @PrimaryKey
+    val id: Int,
+
+    @ColumnInfo(name = "owner")
     val owner: GitHubOwner,
+
+    @ColumnInfo(name = "repo_name")
     @Json(name = "name")
     val repoName: String,
+
+    @ColumnInfo(name = "html_url")
     @Json(name = "html_url")
     val url: String,
+
+    @ColumnInfo(name = "stargazers_count")
     @Json(name = "stargazers_count")
     val star: String
 )
