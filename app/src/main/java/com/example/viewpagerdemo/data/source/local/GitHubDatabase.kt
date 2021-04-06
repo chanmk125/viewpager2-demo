478614fabba4bc3e16f4af7a5668e58b8a36b29e
mrky125 <4ex21ia@gmail.com>
2021-04-07 02:02:17 +0900

    add annotation typeconverter to databse

--- app/src/main/java/com/example/viewpagerdemo/data/source/local/GitHubDatabase.kt
+++ app/src/main/java/com/example/viewpagerdemo/data/source/local/GitHubDatabase.kt
@@ -2,9 +2,12 @@ package com.example.viewpagerdemo.data.source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.viewpagerdemo.data.model.GitHubRepo
+import com.example.viewpagerdemo.data.model.converter.GitHubRepoConverter
 
 @Database(entities = [GitHubRepo::class], version = 1)
+@TypeConverters(GitHubRepoConverter::class)
 abstract class GitHubDatabase : RoomDatabase() {
 
     abstract fun repoDao(): RepoDao
